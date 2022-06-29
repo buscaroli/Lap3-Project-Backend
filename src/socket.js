20f601734af6bf1b314b6f93a8ca7b59082ce5a6
mabus <m.buscaroli@outlook.com>
2022-06-29 14:38:55 +0100

    emove socket.on nextQuestion and add socket.on gameover

--- src/socket.js
+++ src/socket.js
@@ -92,37 +92,34 @@ io.on('connection', (socket) => {
   // client asks for the next question (while sending the score for the previous Question eg 0, -1, +2)
   // if there are no more questions server should return
   // the local score and the top scores from the DB (to be implemented)
-  socket.on('retrieveQuestion', ({ questionScore }) => {
-    console.log('socket.js - questionScore -> ', questionScore)
+  // socket.on('retrieveQuestion', ({ questionScore }) => {
+  //   console.log('socket.js - questionScore -> ', questionScore)
 
-    player.updatePlayerScore({ questionScore, score: player.getPlayerScore() })
-    console.log('playerScore ')
-    const question = game.nextQuestion()
-    if (question) {
-      io.emit('ready', { question, playersData: Game.players })
-    } else {
-      console.log('No questions left')
-      // add player's details to the DB
-      // comment out until DB setup properly
-      console.log('*=*=* ', player)
-      console.log('AAAAAAAA ', Game.players)
-      Game.addScoreToDatabase(player)
+  //   player.updatePlayerScore({ questionScore, score: player.getPlayerScore() })
+  //   console.log('playerScore ')
+  //   // const question = game.nextQuestion()
+  //   if (question) {
+  //     io.emit('ready', { question, playersData: Game.players })
+  //   } else {
+  //     console.log('No questions left')
+  //     // add player's details to the DB
+  //     // comment out until DB setup properly
+  //     console.log('*=*=* ', player)
+  //     console.log('AAAAAAAA ', Game.players)
+  //     Game.addScoreToDatabase(player)
 
-      // tell the clients that there are no questions left
-      // and return their score for the current game
-      socket.emit('noQuestionsLeft', {
-        score: player.getPlayerScore(),
-        playersData: Game.players,
-      })
+  //     // tell the clients that there are no questions left
+  //     // and return their score for the current game
+  //     socket.emit('noQuestionsLeft', {
+  //       score: player.getPlayerScore(),
+  //       playersData: Game.players,
+  //     })
 
-      //reset the player's score in case they are staying for another game without disconnecting/reconnecting
-      player.resetPlayerScore()
-    }
-  })
+  //     //reset the player's score in case they are staying for another game without disconnecting/reconnecting
+  //     player.resetPlayerScore()
+  //   }
 
-  //
-  // won't be used, kept just for debugging
-  socket.on('chat message', (msg) => {
-    console.log('message: ', msg)
+  socket.on('gameover', () => {
+    player.resetPlayerScore()
   })
 })
