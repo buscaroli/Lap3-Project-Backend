8f37b798e6686b6e71327e965382714026e13ba4
mabus <m.buscaroli@outlook.com>
2022-06-29 19:04:26 +0100

    bug fix

--- src/models/player.js
+++ src/models/player.js
@@ -21,7 +21,7 @@ class Player {
   }
 
   updatePlayerName(name) {
-    console.log('Updading player name to ', name)
+    console.log('Updating player name to ', name)
     this.name = name
   }
 }
