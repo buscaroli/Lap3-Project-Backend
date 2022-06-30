385788ae9080f8f9878934b31daa25b6b1820330
mabus <m.buscaroli@outlook.com>
2022-06-30 18:51:18 +0100

    send a basic page with a 404 message for users trying to pink non-existent endpoints

--- src/server.js
+++ src/server.js
@@ -15,6 +15,12 @@ app.use(express.static(publicDirPath))
 const scoreRoutes = require('./controllers/scores')
 app.use('/scores', scoreRoutes)
 
+app.get('*', (req, res) => {
+  res.send(
+    '<h1>404</h1><h4>Page Not Found</h4><style>body{ margin: 30% 30%;}h1,h4{color: red;}h1{font-size: 6rem;}h4{font-size:3rem}</style>'
+  )
+})
+
 const server = http.createServer(app)
 
 module.exports = server
